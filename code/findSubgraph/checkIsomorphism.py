ca5549c0ea34d3106cf4cb4b8488312b62d5b321
Son <haison120404@gmail.com>
2023-12-28 20:24:03 +0700

    28/12/2023

--- code/findSubgraph/checkIsomorphism.py
+++ code/findSubgraph/checkIsomorphism.py
@@ -13,8 +13,8 @@ def read_graph_from_tsv(file_path):
     return G
 
 # Đường dẫn đến file TSV cục bộ
-file_path1 = 'Graph.edgelist'
-file_path2 = 'subGraph.edgelist'
+file_path1 = 'test_2.edgelist'
+file_path2 = 'test_1.edgelist'
 
 # Đọc đồ thị từ file TSV
 my_graph = read_graph_from_tsv(file_path1)
@@ -23,7 +23,18 @@ pattern_graph = read_graph_from_tsv(file_path2)
 # Tiếp tục xử lý subgraph isomorphism như trong mã gốc của bạn
 subgraph_isomorphisms = list(nx.algorithms.isomorphism.GraphMatcher(my_graph, pattern_graph).subgraph_isomorphisms_iter())
 
-# Kiểm tra xem có đồ thị con đồng đẳng hay không
+formatted_subgraphs = []
+for subgraph in subgraph_isomorphisms:
+    formatted_subgraph_str = ', '.join(f'{pattern_node} {my_graph_node}' for pattern_node, my_graph_node in subgraph.items())
+    formatted_subgraphs.append(formatted_subgraph_str)
+
+formatted_subgraphs_str = '\n'.join(formatted_subgraphs)
+
+# Write formatted subgraph isomorphisms to a text file
+with open('formatted_subgraphs.txt', 'w') as f:
+    f.write(formatted_subgraphs_str)
+
+# Check if there are any subgraph isomorphisms
 if subgraph_isomorphisms:
     print("Có một đồ thị con đồng đẳng.")
 else:
--- code/findSubgraph/findSubgraph.py
+++ code/findSubgraph/findSubgraph.py
@@ -1,6 +1,12 @@
 import networkx as nx
-import matplotlib.pyplot as plt
+import math
+from networkx.algorithms import isomorphism
+from pyvis.network import Network
+from IPython.display import display, HTML
 
+
+
+#==========================[READ FILE FUNCTION]============================#
 def read_graph_from_tsv(file_path):
     G = nx.Graph()
     with open(file_path, 'r') as file:
@@ -8,15 +14,18 @@ def read_graph_from_tsv(file_path):
         for line in lines:
             parts = line.strip().split()
             if len(parts) >= 2:
-                u, v = map(int, parts[:2])  # Giữ lại chỉ số đỉnh
-                weight = float(parts[-1].split(':')[-1].strip('}'))  # Chuyển đổi trọng số từ chuỗi thành số
+                # load source vertix, destination vertix, and weight
+                u, v, weight = map(int, parts[:3])  
+                G.add_node(u)
+                G.add_node(v)
                 G.add_edge(u, v, weight=weight)
     return G
+#==========================================================================#
 
+file_path1 = 'testcase_1_G.edgelist' # File graph G
+file_path2 = 'testcase_1_H.edgelist' # File graph H
 
-file_path1 = 'Graph.edgelist'
-file_path2 = 'subGraph.edgelist'
-# Read graph from TSV file
+# Read graph from file
 my_graph = read_graph_from_tsv(file_path1)
 
 # Define a small pattern graph for subgraph isomorphism
@@ -24,32 +33,62 @@ pattern_graph = read_graph_from_tsv(file_path2)
 
 # Find subgraph isomorphism using NetworkX
 subgraph_isomorphisms = list(nx.algorithms.isomorphism.GraphMatcher(my_graph, pattern_graph).subgraph_isomorphisms_iter())
+if (subgraph_isomorphisms):
+    print("There are subgraph isomorphism")
+else:
+    print("There are not subgraph isomorphism")
+    exit()
+# Load the number nodes of pattern_graph 
+num_nodes =pattern_graph.number_of_nodes()
 
-# Print the subgraph isomorphisms
-# print("Subgraph Isomorphisms:")
-# for subgraph in subgraph_isomorphisms:
-#     print(subgraph)
+# Count the number of subgraphs
+count = 0
+for subgraph in subgraph_isomorphisms:
+    count += 1
+# Because the order of the nodes does not matter
+count=count/math.factorial(num_nodes) 
+# Print the number of subgraphs
+print("Number of subgraph isomorphisms:", count)
+
+# Identify nodes in a found subgraph
+pos=0
+list_nodes=[]
+subgraph=str(subgraph_isomorphisms[0])
+for i in range(num_nodes):
+    if(pos==0): 
+        pos=subgraph.find(':')
+    else:
+        pos = subgraph.find(':', pos + 1)
+    if(pos!=-1):
+        list_nodes.append(int(subgraph[pos-1]))
+
+# Set color the main graph the matched subgraphs 
+for i in my_graph.nodes:
+    my_graph.nodes[i]['color']='#00BFFF'
+for i in list_nodes:
+    my_graph.nodes[i]['color']='red'
+for edge in my_graph.edges:
+    my_graph.edges[edge]['color'] = '#00BFFF'
+for i in list_nodes:
+    for j in list_nodes:
+        if my_graph.has_edge(i, j):
+            my_graph.edges[(i, j)]['color'] = 'red'
 
 # Visualize the main graph and the matched subgraphs
-pos = nx.spring_layout(my_graph, k=0.15, iterations=100) 
+net = Network(
+    notebook=True,
+    cdn_resources="remote",
+    bgcolor="white",
+    font_color="750px",
+    height="750px",
+    width="100%",
+    select_menu=True,
+    filter_menu=True,
+)
 
-nx.draw(my_graph, pos, node_size=20,  
-        with_labels=False,
-        font_size=5, 
-        node_color='skyblue',
-        alpha = 0.7, 
-        font_weight='regular',
-        linewidths=0.2, 
-        edge_color='0.3')
-
-nx.draw(pattern_graph, pos,  
-        node_size=100,
-        font_size=5,
-        node_color='red',
-        font_weight='regular',
-        edge_color='red', 
-        linewidths=1)
-
-plt.axis("off")
-plt.savefig("output1.png",dpi=3000)
-
+net.from_nx(my_graph)
+net.toggle_physics(False)
+display(HTML(net.generate_html()))
+# Save the html file of the graph by the name "networkx_pyvis.html"
+net.save_graph("networkx_pyvis.html")
+HTML(filename="networkx_pyvis.html")
